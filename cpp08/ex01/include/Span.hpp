0d562a9cea0f3c2786d7525bf4ab541173c19f44
Peters <pehenri2@c1r7p3.42sp.org.br>
2024-11-13 14:44:46 -0300

    starting cpp08/ex01

---
+++ cpp08/ex01/include/Span.hpp
@@ -0,0 +1,17 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+class Span {
+public:
+	Span();
+	Span(unsigned int n);
+	Span(Span const &src);
+	~Span();
+	Span &operator=(Span const &rhs);
+
+	void addNumber(int n);
+	int shortestSpan();
+	int longestSpan();
+}
+
+#endif
