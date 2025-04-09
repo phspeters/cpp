c4248a843f5fd9f205fff60e20970ecb1bb83665
Peters <pehenri2@c1r7p3.42sp.org.br>
2025-04-09 15:55:14 -0300

    ex01 improved error messaging

--- cpp09/ex01/src/RPN.cpp
+++ cpp09/ex01/src/RPN.cpp
@@ -27,7 +27,7 @@ void RPN::solveExpression(std::string expression) {
 	while (iss >> token) {
 		if (token == "+" || token == "-" || token == "*" || token == "/") {
 			if (_stack.size() < 2) {
-				std::cout << "Error: not enough operands" << std::endl;
+				std::cout << "Error: not enough operands to perform operation "<< token << std::endl;
 				return;
 			}
 
@@ -79,10 +79,18 @@ void RPN::_validateExpression(std::string expression) {
 	}
 
 	if (operands > operators + 1) {
-		std::cout << "Error: missing " << operands - (operators + 1) << " operators" << std::endl;
+		std::cout << "Error: RPN expression is malformed - too many operands (" << operands 
+		<< " operands, " << operators << " operators)" << std::endl
+		<< "In valid RPN notation, with n operands you need exactly (n-1) operators" << std::endl
+		<< "Please either add " << operands - (operators + 1) << " more operators or remove "
+		<< operands - (operators + 1) << " operands" << std::endl;
 		exit(1);
 	} else if (operands < operators + 1) {
-		std::cout << "Error: missing " << (operators + 1)  - operands << " operands" << std::endl;
+		std::cout << "Error: RPN expression is malformed - too many operators (" << operators 
+		<< " operators, " << operands << " operands)" << std::endl
+		<< "In valid RPN notation, with n operands you need exactly (n-1) operators" << std::endl
+		<< "Please either add " << (operators + 1) - operands << " more operands or remove "
+		<< (operators + 1) - operands << " operators" << std::endl;
 		exit(1);
 	}
 }
